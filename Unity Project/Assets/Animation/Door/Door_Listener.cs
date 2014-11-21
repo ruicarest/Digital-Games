1930fcf0f1e2d9d6de09dd7609b540ace8b19120
Rui Cardoso Esteves <ruicarest@gmail.com>
2014-11-21 10:41:58 +0000

    New Test Scene

---
+++ Unity Project/Assets/Animation/Door/Door_Listener.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door_Listener : MonoBehaviour {
+
+	public AnimationClip Closing; 
+	public AnimationClip Openning; 
+
+	private bool Isopen = false;
+
+	// Use this for initialization
+	void Start () {
+
+	 
+	}
+
+	void Start_Action(float damage) {
+		print (damage);
+
+		if (!animation.isPlaying) {
+			if(Isopen == false){
+			animation.Play (Openning.name);
+			Isopen = true;
+			}
+			else if (Isopen == true)
+			{
+			//animation.Play ("Door_Closing");
+			animation.Play(Closing.name);
+			Isopen = false;
+			}
+		
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity Project/Assets/Scripts/Player/ActionClick.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ActionClick : MonoBehaviour {
+
+	new private RaycastHit hit;
+	new private Ray ray;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+
+				ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+				if (Physics.Raycast (ray, out hit, 1F)) {
+				print (hit.collider.gameObject.name);
+
+				hit.collider.gameObject.SendMessage("Start_Action",1);
+
+				}
+				
+		}
+
+
+	}
+}
